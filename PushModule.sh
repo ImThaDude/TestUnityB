ec106e814c5c343d00c043e8e4395c74a42caebd
George Quan <contact@gpquanli.com>
2021-04-12 17:51:29 -0700

    Modified module sh.

--- PushModule.sh
+++ PushModule.sh
@@ -1,18 +1,21 @@
 if [ -z "$1" ]; then
-	echo "Comment needed for push compilation into module."
+	echo "Expected input ./pm [Comment]"
 	exit 0
 fi
 echo "Creating module push with comment: $1"
 git checkout module
 git rebase master
+mv Assets/* ./
 git reset origin/module
 git add .
 git commit -m $1
 git push origin module
 git checkout master
 
+#Plugins tries to put everything in the lower level to easily merge plugins.
 git checkout plugins
 git checkout master Assets/Plugins
+mv Assets/Plugins/* ./
 git add .
 git commit -m $1 
 git push pluginsref plugins
