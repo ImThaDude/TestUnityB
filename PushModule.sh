ba498a6d5f9f9a50983c81c8822f4f3c5729ab52
George Quan <contact@gpquanli.com>
2021-04-12 18:40:46 -0700

    Added last additions for longer comments.

--- PushModule.sh
+++ PushModule.sh
@@ -2,13 +2,17 @@ if [ -z "$1" ]; then
 	echo "Expected input ./pm [Comment]"
 	exit 0
 fi
-echo "Creating module push with comment: $1"
+$comment = $1
+$1 = 0
+$2 = 0
+$3 = 0
+echo "Creating module push with comment: $comment"
 git checkout module
 git rebase master
 mv Assets/* ./
 git reset origin/module
 git add .
-git commit -m $1
+git commit -m $comment
 git push origin module
 git checkout master
 git add .
@@ -19,7 +23,7 @@ git checkout plugins
 git checkout master Assets/Plugins
 mv Assets/Plugins/* ./
 git add .
-git commit -m $1 
+git commit -m $comment
 git push pluginsref plugins
 git checkout master
 git add .
